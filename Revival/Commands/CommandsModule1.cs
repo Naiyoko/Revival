c7eb3cf0b61b703d8c3fba8e9cbabc8fe7ca8d89
gokur <gokur@MAFUYU>
2022-07-30 10:46:10 -0400

    Add project files.

---
+++ Revival/Commands/CommandsModule1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+public class CommandsModule1 : BaseCommandModule
+{
+    [Command("greet")]
+    public async Task GreetCommand(CommandContext ctx)
+    {
+        await ctx.RespondAsync("Here we aree, responding with some random text.");
+    }
+
+    [Command("sayhi")]
+    public async Task SayhiCommand(CommandContext ctx, [RemainingText] string name)
+    {
+        await ctx.RespondAsync($"Helloo, {name}~");
+    }
+
+    [Command("rannumber")]
+    public async Task RannumberCommand(CommandContext ctx, int min, int max)
+    {
+        var random = new Random();
+        await ctx.RespondAsync($"Your number is: {random.Next(min, max)}");
+
+    }
+
+    [Command("RepTest")]
+    public async Task ReptestCommand(CommandContext ctx)
+    {
+        var msg = await new DiscordMessageBuilder()
+            .WithContent($"Testing, testing! Do we have a reply, Nayoko?")
+            .WithReply(ctx.Message.Id)
+            .SendAsync(ctx.Channel);
+    }
+
+    [Command("EmbedTest")]
+    public async Task EmbedtestCommand(CommandContext ctx)
+    {
+        var msg = new DiscordEmbedBuilder()
+            .WithAuthor("Test")
+            .WithTitle("Another test")
+            .WithImageUrl("https://c.tenor.com/BoYBoopIkBcAAAAC/anime-smash.gif");
+        await ctx.RespondAsync(msg);
+    }
+}
---
+++ Revival/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace Revival
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        internal static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                MinimumLogLevel = LogLevel.Debug,
+                LogTimestampFormat = "MMM dd yyyy - hh:mm:ss tt",
+                Token = "nope!",
+                TokenType = TokenType.Bot,
+            });
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { ".." }
+            });
+
+            commands.RegisterCommands<CommandsModule1>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
